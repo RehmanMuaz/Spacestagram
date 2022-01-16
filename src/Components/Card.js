1302683ac6195d2ac42df357f56a7795eb265d56
Muaz Rehman <muaz_r5@Hotmail.com>
2022-01-15 19:13:44 -0500

    Added Expanding Text. Adjusted Layout. Added Button Hover Effects

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,6 +4,25 @@ import React, { useState } from 'react'
 import { FaHeart,FaRegHeart } from 'react-icons/fa'
 import { BiShare } from 'react-icons/bi'
 import { useAlert } from 'react-alert'
+import { MdExpandMore, MdExpandLess } from 'react-icons/md';
+
+const ExpandText = ({ children }) => {
+    const content = children;
+    const [isExpanded, setExpanded] = useState(true);
+
+    const toggleExpand = () => {
+      setExpanded(!isExpanded);
+    };
+    return (
+      <p>
+        {isExpanded ? content.slice(0, 150) : content}
+        <span onClick={toggleExpand} className="read-or-hide">
+          {isExpanded ? <div className='ExpandBtn'>Expand<MdExpandMore style={{verticalAlign: 'middle'}}/></div> : <div className='ExpandBtn'>Collapse<MdExpandLess style={{verticalAlign: 'middle'}}/></div>}
+        </span>
+      </p>
+    );
+  };
+
 
 function Card(props) {
     const [liked, setLike] = useState(false);
@@ -15,17 +34,17 @@ function Card(props) {
             <div className='Content'>
                 <h2>{props.title}</h2>
                 <h3>{props.date}</h3>
-                <p>{props.desc}</p>
+                <p>{<ExpandText>{props.desc}</ExpandText>}</p>
                 <div className='ButtonRow'>
                     <button 
                         onClick={() => setLike(!liked)}
                     >
-                        {liked ? <FaHeart color='red' size={25}/> : <FaRegHeart size={25} color='grey'/>}
+                        {liked ? <FaHeart className='LikeBtnClicked' size={25}/> : <FaRegHeart className='LikeBtnUnclicked' size={25}/>}
                     </button>
                     <button 
                         onClick={() => {navigator.clipboard.writeText(props.url); alert.success('Link Copied')}}
                     >
-                        <BiShare size={25} color='grey'/>
+                        <BiShare className='ShareBtn' size={25}/>
                     </button>
                 </div>
             </div>
